7ab301e252207f0e8026d79e7bf28bfd4f71be0b
Ricardo Taliuli <158619524+taliuli10@users.noreply.github.com>
2024-06-06 07:44:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+function registrarAluno(e) {
+    e.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const matricula = document.getElementById('matricula').value;
+    const nota1 = parseFloat(document.getElementById('nota1').value);
+    const nota2 = parseFloat(document.getElementById('nota2').value);
+    const media = (nota1 + nota2) / 2;
+    const situacao = media >= 5 ? "Aprovado" : "Reprovado";
+    const linha = criarLinha(nome, matricula, nota1, nota2, media, situacao);
+    adicionarLinha(linha);
+    limparCamposFormulario();
+}
+function criarLinha(nome, matricula, nota1, nota2, media, situacao) {
+    const linha = document.createElement('tr');
+    linha.innerHTML = `
+        <td>${nome}</td>
+        <td>${matricula}</td>
+        <td>${nota1}</td>
+        <td>${nota2}</td>
+        <td>${media.toFixed(2)}</td>
+        <td>${situacao}</td>
+    `;
+    return linha;
+}
+function adicionarLinha(linha) {
+    const tbody = document.querySelector('#tabela tbody');
+    tbody.appendChild(linha);
+}
+function limparCamposFormulario() {
+    document.getElementById('nome').value = '';
+    document.getElementById('matricula').value = '';
+    document.getElementById('nota1').value = '';
+    document.getElementById('nota2').value = '';
+}
+function limparTabela() {
+    const tbody = document.querySelector('#tabela tbody');
+    tbody.innerHTML = '';
+}
+
+document.getElementById('formulario').addEventListener('submit', registrarAluno);
+document.getElementById('limparTabela').addEventListener('click', limparTabela);
+
+
